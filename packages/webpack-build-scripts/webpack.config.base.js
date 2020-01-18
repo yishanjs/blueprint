4e0351275f6a175fb72e70e6606d50194dffea76
qy <xiangyujames@outlook.com>
2020-01-19 00:45:42 +0800

    v4.0.1

--- packages/webpack-build-scripts/webpack.config.base.js
+++ packages/webpack-build-scripts/webpack.config.base.js
@@ -33,102 +33,99 @@ const PACKAGE_NAME = getPackageName();
  * Configure plugins loaded based on environment.
  */
 const plugins = [
-    // Used for async error reporting
-    // Can remove after https://github.com/webpack/webpack/issues/3460 resolved
-    new CheckerPlugin(),
+  // Used for async error reporting
+  // Can remove after https://github.com/webpack/webpack/issues/3460 resolved
+  new CheckerPlugin(),
 
-    // CSS extraction is only enabled in production (see scssLoaders below).
-    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  // CSS extraction is only enabled in production (see scssLoaders below).
+  new MiniCssExtractPlugin({ filename: "[name].css" }),
 
-    // TODO: enable this
-    // Zero tolereance for circular depenendencies
-    // new CircularDependencyPlugin({
-    //     exclude: /.js|node_modules/,
-    //     failOnError: true,
-    // }),
+  // TODO: enable this
+  // Zero tolereance for circular depenendencies
+  // new CircularDependencyPlugin({
+  //     exclude: /.js|node_modules/,
+  //     failOnError: true,
+  // }),
 ];
 
 if (!IS_PRODUCTION) {
-    plugins.push(
-        // Trigger an OS notification when the build succeeds in dev mode.
-        new WebpackNotifierPlugin({ title: PACKAGE_NAME })
-    );
+  plugins.push(
+    // Trigger an OS notification when the build succeeds in dev mode.
+    new WebpackNotifierPlugin({ title: PACKAGE_NAME }),
+  );
 }
 
 // Module loaders for .scss files, used in reverse order:
 // compile Sass, apply PostCSS, interpret CSS as modules.
 const scssLoaders = [
-    // Only extract CSS to separate file in production mode.
-    IS_PRODUCTION ? MiniCssExtractPlugin.loader : require.resolve("style-loader"),
-    {
-        loader: require.resolve("css-loader"),
-        options: {
-            // necessary to minify @import-ed files using cssnano
-            importLoaders: 1,
-        },
+  // Only extract CSS to separate file in production mode.
+  IS_PRODUCTION ? MiniCssExtractPlugin.loader : require.resolve("style-loader"),
+  {
+    loader: require.resolve("css-loader"),
+    options: {
+      // necessary to minify @import-ed files using cssnano
+      importLoaders: 1,
     },
-    {
-        loader: require.resolve("postcss-loader"),
-        options: {
-            plugins: [
-                require("autoprefixer"),
-                require("cssnano")({ preset: "default" }),
-            ],
-        },
+  },
+  {
+    loader: require.resolve("postcss-loader"),
+    options: {
+      plugins: [require("autoprefixer"), require("cssnano")({ preset: "default" })],
     },
-    require.resolve("sass-loader"),
+  },
+  require.resolve("sass-loader"),
 ];
 
 module.exports = {
-    devtool: IS_PRODUCTION ? false : "inline-source-map",
+  devtool: IS_PRODUCTION ? false : "inline-source-map",
 
-    devServer: {
-        contentBase: "./src",
-        disableHostCheck: true,
-        historyApiFallback: true,
-        https: false,
-        // TODO: enable HMR
-        // hot: true,
-        index: path.resolve(__dirname, "src/index.html"),
-        inline: true,
-        stats: "errors-only",
-        open: false,
-        overlay: {
-            warnings: true,
-            errors: true,
+  devServer: {
+    contentBase: "./src",
+    disableHostCheck: true,
+    historyApiFallback: true,
+    https: false,
+    // TODO: enable HMR
+    // hot: true,
+    index: path.resolve(__dirname, "src/index.html"),
+    inline: true,
+    stats: "errors-only",
+    open: false,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+    port: DEV_PORT,
+  },
+
+  mode: IS_PRODUCTION ? "production" : "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: require.resolve("awesome-typescript-loader"),
+        options: {
+          configFileName: "./src/tsconfig.json",
         },
-        port: DEV_PORT,
-    },
+      },
+      {
+        test: /\.scss$/,
+        use: scssLoaders,
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg|png|gif|jpe?g)$/,
+        loader: require.resolve("file-loader"),
+        options: {
+          name: "[name].[ext]?[hash]",
+          outputPath: "assets/",
+        },
+      },
+    ],
+  },
 
-    mode: IS_PRODUCTION ? "production" : "development",
+  plugins,
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: require.resolve("awesome-typescript-loader"),
-                options: {
-                    configFileName: "./src/tsconfig.json",
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: scssLoaders,
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2|svg|png|gif|jpe?g)$/,
-                loader: require.resolve("file-loader"),
-                options: {
-                    name: "[name].[ext]?[hash]",
-                    outputPath: "assets/",
-                },
-            },
-        ],
-    },
-
-    plugins,
-
-    resolve: {
-        extensions: [ ".js", ".jsx", ".ts", ".tsx", ".scss" ],
-    },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"],
+  },
 };
